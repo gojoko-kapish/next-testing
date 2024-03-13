15395a4333d2be6d5bb03dbb40c2f9d39fcda531
kapish singh <kapish.singh@gojokotech.com>
2024-03-13 18:19:36 +0530

    Added Lang

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-import Headers from "@/components/common/Header/headers";
-
-export default function Home() {
-  return (
-    <main>
-      <Headers />
-    </main>
-  );
-}
--- components/common/Header/headers.tsx
+++ components/common/Header/headers.tsx
@@ -3,21 +3,25 @@ import { MainNavigationMenu } from "./main-menu";
 import { ThemeToggle } from "@/components/common/Theme/theme";
 import { assets } from "@/assets";
 import Image from "next/image";
+import LocaleSwitcher from "@/components/locale-switcher";
 
 const Headers = () => {
   return (
-    <div className="max-w-7xl mx-auto">
-        <div className="flex justify-between">
-      <Image
-        className="w-10 rounded-full md:block hidden"
-        src={assets.header}
-        alt={"alt"}
-        width={16}
-        height={16}
-      />
-      <MainNavigationMenu />
-      <ThemeToggle />
-    </div>
+    <div className="fixed top-0 left-0 w-full h-auto z-50 bg-white shadow-md ">
+      <div className="max-w-7xl mx-auto flex justify-between px-4 py-2">
+        <Image
+          className="w-10 rounded-full md:block hidden"
+          src={assets.header}
+          alt={"alt"}
+          width={16}
+          height={16}
+        />
+        <MainNavigationMenu />
+        <div className="flex gap-2">
+          <ThemeToggle />
+          <LocaleSwitcher />
+        </div>
+      </div>
     </div>
   );
 };
